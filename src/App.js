65dce60e1dc00177c2ab98e3f84bb4d1890ba9b6
kimsong <saokismong@gmail.com>
2023-12-07 22:30:05 +0700

    Change

--- src/App.js
+++ src/App.js
@@ -5,23 +5,22 @@ function App() {
         <div className="col col-xl-10">
           <div className="card" >
             <div className="card-body p-5">
-              <h6 className="mb-3">Awesome Todo List</h6>
+              <h6 className="mb-3">Todo List</h6>
               <form className="d-flex justify-content-center align-items-center mb-4">
                   <input type="text" id="form3" className="form-control" placeholder="Enter Note..." />
                 <button type="submit" className="btn btn-primary  ms-2">Add</button>
               </form>
               <ul className="list-group mb-0">
                 
-                <li
+                {/* <li
                   className="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
                   <div className="d-flex align-items-center">
-                    <input className="form-check-input me-2" type="checkbox" value="" aria-label="..." />
                     Morbi leo risus
                   </div>
                   <a href="#!" title="Remove item">
-                    <i className="fas fa-times text-primary">ss</i>
+                    <i className="fa fas fa-times text-primary"></i>
                   </a>
-                </li>
+                </li> */}
               </ul>
   
             </div>
